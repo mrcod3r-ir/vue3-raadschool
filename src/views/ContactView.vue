1efbe00808cd625c8937f99b7ce5435c5a60243a
mr cod3r <mrcod3r.ir@gmail.com>
2022-05-09 09:50:33 +0430

    init = vue3 tailwindcss

---
+++ src/views/ContactView.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="contact">
+		<h1 class="text-3xl font-bold underline text-purple-400">Hello tailwindcss !</h1>
+	</div>
+</template>
+
+<style>
+	@media (min-width: 1024px) {
+		.about {
+			min-height: 100vh;
+			display: flex;
+			align-items: center;
+		}
+	}
+</style>
