8e54bb571a31369740ca4cbcdb2cb5d98e480291
mr cod3r <mrcod3r.ir@gmail.com>
2022-05-14 19:31:06 +0430

    active pinia | toggle sidebar | fix main content margin

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,26 @@
 	import HelloWorld from "@/components/HelloWorld.vue"
 	import { onMounted, reactive } from "vue"
 	import { themeChange } from "theme-change"
-	import { HomeIcon, AnnotationIcon, ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from "@heroicons/vue/solid"
+	import { HomeIcon, AnnotationIcon, ChevronDoubleLeftIcon, ChevronDoubleRightIcon, PhotographIcon, PhoneIcon } from "@heroicons/vue/solid"
 	import Navbar from "@/components/Navbar.vue"
+	import { useMainStore } from "./stores/main"
+	const mainStore = useMainStore()
 	const data = reactive({
 		sidebar: false
 	})
 	onMounted(() => {
 		themeChange(false)
 	})
+	function toggleSidebar() {
+		console.log("toggleSidebar")
+		console.log(mainStore.sidebarExpanded)
+	}
 </script>
 
 <template>
 	<div class="drawer">
 		<input id="toggleSidebar" type="checkbox" class="drawer-toggle" v-model="data.sidebar" />
-		<div class="drawer-content">
+		<div :class="['drawer-content', mainStore.sidebarExpanded ? 'mr-36' : 'mr-16']">
 			<!-- Page content here -->
 			<header>
 				<Navbar>
@@ -30,26 +36,26 @@
 			</main>
 			<footer></footer>
 		</div>
-		<div :class="['drawer-side transition-all relative', data.sidebar ? 'w-60' : 'w-16']">
+		<div :class="['drawer-side transition-all relative', mainStore.sidebarExpanded ? 'w-36' : 'w-16']">
 			<ul class="menu p-4 overflow-y-auto w-full bg-base-100 text-base-content bg-slate-700 text-slate-300 overflow-x-hidden">
 				<!-- Sidebar content here -->
 				<li>
-					<RouterLink class="px-0" to="">
-						<ChevronDoubleRightIcon v-if="data.sidebar" class="w-5 h-5" />
+					<RouterLink class="px-0 flex justify-center" to="" @click="mainStore.toggleSidebarExpanded()">
+						<ChevronDoubleRightIcon v-if="mainStore.sidebarExpanded" class="w-5 h-5" />
 						<ChevronDoubleLeftIcon v-else class="w-5 h-5" />
 					</RouterLink>
 				</li>
 				<li>
-					<RouterLink class="px-0" to="/"><HomeIcon class="h-5 w-5" /><span v-if="data.sidebar">Home</span></RouterLink>
+					<RouterLink class="px-0 flex justify-center" to="/"><HomeIcon class="h-5 w-5" /><span v-if="mainStore.sidebarExpanded">Home</span></RouterLink>
 				</li>
 				<li>
-					<RouterLink class="px-0" to="/about"><AnnotationIcon class="h-5 w-5" /><span v-if="data.sidebar">About</span></RouterLink>
+					<RouterLink class="px-0 flex justify-center" to="/about"><AnnotationIcon class="h-5 w-5" /><span v-if="mainStore.sidebarExpanded">About</span></RouterLink>
 				</li>
 				<li>
-					<RouterLink class="px-0" to="/contact"><span v-if="data.sidebar">Contact</span></RouterLink>
+					<RouterLink class="px-0 flex justify-center" to="/contact"><PhoneIcon class="h-5 w-5" /><span v-if="mainStore.sidebarExpanded">Contact</span></RouterLink>
 				</li>
 				<li>
-					<RouterLink class="px-0" to="/photos"><span v-if="data.sidebar">Photos</span></RouterLink>
+					<RouterLink class="px-0 flex justify-center" to="/photos"><PhotographIcon class="h-5 w-5" /><span v-if="mainStore.sidebarExpanded">Photos</span></RouterLink>
 				</li>
 			</ul>
 		</div>
