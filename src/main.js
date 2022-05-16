36e81ca22c3eaebd837f30817c5ecb0d5777af40
mr cod3r <mrcod3r.ir@gmail.com>
2022-05-16 13:51:02 +0430

    add vazirmatn font for test persian font

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import App from './App.vue'
-import router from './router'
-import './assets/index.css'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
+import axios from "axios"
+import VueAxios from "vue-axios"
+import App from "./App.vue"
+import router from "./router"
+import "./assets/index.css"
+import "@fontsource/vazirmatn"
 const app = createApp(App)
 
 app.use(VueAxios, axios)
 app.use(createPinia())
 app.use(router)
-app.mount('#app')
+app.mount("#app")
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,35 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require("daisyui")
-  ],
-   // daisyUI config (optional)
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    // rtl: true,
-    prefix: "",
-    themes: ["light", "dark"],
-    // darkTheme: "dark",
-  },
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {}
+	},
+	plugins: [require("daisyui")],
+	// daisyUI config (optional)
+	daisyui: {
+		styled: true,
+		themes: true,
+		base: true,
+		utils: true,
+		logs: true,
+		// rtl: true,
+		prefix: "",
+		themes: [
+			"light",
+			"dark",
+			{
+				raad: {
+					primary: "#7e22ce",
+					secondary: "#059669",
+					accent: "#fcd34d",
+					neutral: "#57534e",
+					"base-100": "#FFFFFF",
+					info: "#fb923c",
+					success: "#5eead4",
+					warning: "#fcd34d",
+					error: "#EA4034"
+				}
+			}
+		]
+		// darkTheme: "dark"
+	}
 }
