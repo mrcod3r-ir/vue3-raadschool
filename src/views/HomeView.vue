58a1b5c039ed682a03b51671fee502ecffe9129c
mr cod3r <mrcod3r.ir@gmail.com>
2022-05-25 14:12:50 +0430

    add gsap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
-	<h1 class="font-bold text-3xl">متن تستی</h1>
+	<h1 class="font-bold text-3xl" ref="title">متن تستی</h1>
 	<h2>main home page</h2>
-	<p>
+	<p ref="p">
 		لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز
 		و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان
 		رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل
@@ -18,6 +18,7 @@
 <script>
 	import NumberPicker from "../components/NumberPicker.vue"
 	import Counter from "../components/Counter.vue"
+	import { gsap } from "gsap"
 	export default {
 		components: { NumberPicker, Counter },
 		data() {
@@ -25,6 +26,10 @@
 				counter: 0,
 				city: "Qom"
 			}
+		},
+		mounted() {
+			gsap.from(this.$refs.title, { x: 300, duration: 1 })
+			gsap.from(this.$refs.p, { x: -300, duration: 1 })
 		}
 	}
 </script>
